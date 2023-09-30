8aaa87f7e897dc523237010337c0a0700cd832ad
ExLuZiVe53 <imstudent27@gmail.com>
2023-09-30 21:15:44 +0200

    Update api.js

--- src/components/servises/api.js
+++ src/components/servises/api.js
@@ -11,8 +11,6 @@ export const fetchPictures = async (page = 1) => {
 export const findPictureSearch = async (q, page = 1) => {
   const KEY_API = '38871249-2eed7f344cf93ec506352b726';
   const { data } = await axios.get(
-    // `https://pixabay.com/api/?q=${namePic}&page=${page}&key=${KEY_API}&image_type=photo&orientation=horizontal&per_page=12`
-
     `https://pixabay.com/api/?key=${KEY_API}&q=${q}&orientation=horizontal&safesearch=true&image_type=photo&per_page=12&page=${page}`
   );
   return data.hits;
